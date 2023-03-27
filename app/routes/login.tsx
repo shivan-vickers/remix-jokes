7e3f39b3fda239ed56bb9e882b5c2f7fae81703d
shivan-vickers <shivan.vickers@gmail.com>
2023-03-26 21:43:32 -0700

    Add feature to register a new user

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -4,7 +4,7 @@ import { Link, useActionData, useSearchParams } from "@remix-run/react";
 import stylesUrl from "~/styles/login.css";
 import { db } from "~/utils/db.server";
 import { badRequest } from "~/utils/request.server";
-import { login, createUserSession } from "~/utils/session.server";
+import { createUserSession, login, register } from "~/utils/session.server";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesUrl },
@@ -65,7 +65,6 @@ export const action = async ({ request }: ActionArgs) => {
   switch (loginType) {
     case "login": {
       const user = await login({ username, password });
-
       if (!user) {
         return badRequest({
           fieldErrors: null,
@@ -86,13 +85,15 @@ export const action = async ({ request }: ActionArgs) => {
           formError: `User with username ${username} already exists`,
         });
       }
-      // create the user
-      // create their session and redirect to /jokes
-      return badRequest({
-        fieldErrors: null,
-        fields,
-        formError: "Not implemented",
-      });
+      const user = await register({ username, password });
+      if (!user) {
+        return badRequest({
+          fieldErrors: null,
+          fields,
+          formError: `Something went wrong trying to create a new user.`,
+        });
+      }
+      return createUserSession(user.id, redirectTo);
     }
     default: {
       return badRequest({
--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -8,15 +8,21 @@ type LoginForm = {
   password: string;
 };
 
+export async function register({ username, password }: LoginForm) {
+  const passwordHash = await bcrypt.hash(password, 10);
+  const user = await db.user.create({
+    data: { username, passwordHash },
+  });
+  return { id: user.id, username };
+}
+
 export async function login({ username, password }: LoginForm) {
   const user = await db.user.findUnique({
     where: { username },
   });
   if (!user) return null;
-
   const isCorrectPassword = await bcrypt.compare(password, user.passwordHash);
   if (!isCorrectPassword) return null;
-
   return { id: user.id, username };
 }
 
