6e18f7f0f7b8f23e6ce78d238636d538fbcddbd6
shivan-vickers <shivan.vickers@gmail.com>
2023-03-27 19:49:27 -0700

    Add meta tags

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,40 +1,58 @@
-import type { ActionArgs, LoaderArgs } from "@remix-run/node";
+import type { ActionArgs, LoaderArgs, MetaFunction } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import { Link, useCatch, useLoaderData, useParams } from "@remix-run/react";
 
 import { db } from "~/utils/db.server";
 import { getUserId, requireUserId } from "~/utils/session.server";
 
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
+  if (!data) {
+    return {
+      title: "No joke",
+      description: "No joke found",
+    };
+  }
+  return {
+    title: `"${data.joke.name}" joke`,
+    description: `Enjoy the "${data.joke.name}" joke and much more`,
+  };
+};
+
 export const loader = async ({ params, request }: LoaderArgs) => {
   const userId = await getUserId(request);
-  const joke = await db.joke.findUnique({ where: { id: params.jokeId } });
-
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeId },
+  });
   if (!joke) {
-    throw new Response("What a joke! Not found.", { status: 404 });
+    throw new Response("What a joke! Not found.", {
+      status: 404,
+    });
   }
-
-  return json({ joke, isOwner: userId === joke.jokesterId });
+  return json({
+    joke,
+    isOwner: userId === joke.jokesterId,
+  });
 };
 
 export const action = async ({ params, request }: ActionArgs) => {
   const form = await request.formData();
-
   if (form.get("intent") !== "delete") {
     throw new Response(`The intent ${form.get("intent")} is not supported`, {
       status: 400,
     });
   }
-
   const userId = await requireUserId(request);
-  const joke = await db.joke.findUnique({ where: { id: params.jokeId } });
-
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeId },
+  });
   if (!joke) {
-    throw new Response("Can't delete what does not exist", { status: 404 });
+    throw new Response("Can't delete what does not exist", {
+      status: 404,
+    });
   }
   if (joke.jokesterId !== userId) {
     throw new Response("Pssh, nice try. That's not your joke", { status: 403 });
   }
-
   await db.joke.delete({ where: { id: params.jokeId } });
   return redirect("/jokes");
 };
@@ -46,7 +64,7 @@ export default function JokeRoute() {
     <div>
       <p>Here's your hilarious joke:</p>
       <p>{data.joke.content}</p>
-      <Link to={"."}>"{data.joke.name}" Permalink</Link>
+      <Link to=".">{data.joke.name} Permalink</Link>
       {data.isOwner ? (
         <form method="post">
           <button className="button" name="intent" type="submit" value="delete">
@@ -61,7 +79,6 @@ export default function JokeRoute() {
 export function CatchBoundary() {
   const caught = useCatch();
   const params = useParams();
-
   switch (caught.status) {
     case 400: {
       return (
@@ -73,7 +90,7 @@ export function CatchBoundary() {
     case 404: {
       return (
         <div className="error-container">
-          Huh? What the heck is "{params.jokeId}"
+          Huh? What the heck is {params.jokeId}?
         </div>
       );
     }
@@ -88,15 +105,6 @@ export function CatchBoundary() {
       throw new Error(`Unhandled error: ${caught.status}`);
     }
   }
-
-  if (caught.status === 404) {
-    return (
-      <div className="error-container">
-        Huh? What the heck is "{params.jokeId}"
-      </div>
-    );
-  }
-  throw new Error(`Unhandled error: ${caught.status}`);
 }
 
 export function ErrorBoundary() {
